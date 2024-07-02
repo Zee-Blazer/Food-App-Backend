5166027ceb4051000b1d1ea82a0d103960e17aba
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-07-02 12:31:08 +0100

    Having a little problem connecting to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,22 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm'; // For the Database
+import { ConfigModule } from '@nestjs/config'; // For the Environment variables
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: process.env.DB_PASS,
+      database: 'task-app',
+      entities: [],
+  })
+  ],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
